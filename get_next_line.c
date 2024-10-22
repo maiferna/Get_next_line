92e0d07f92947f88f2b5cb91edea1a2a8d1c5616
maiferna <maiderfernandez57@gmail.com>
2024-10-22 15:01:31 +0200

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -12,48 +12,57 @@
 
 #include "get_next_line.h"
 
-/*Esta función se encargará de leer del archivo. 
-Leerá datos en bloques desde el descriptor de archivo hasta que encuentre un salto de línea (\n) 
-o llegue al final del archivo. Debería leer datos del archivo y gestionar stash*/
-
-char	*ft_read_line(int fd, char *stash)
+static char	*ft_read_line(int fd, char *stash)
 {
-	char	*read_line;
+	int		read_line;
+	char	*buffer;
 	char	*temp;
+
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buffer)
+		return (NULL);
+	read_line = 1;
+	if (!stash)
+		stash = ft_strdup("");
+	while (read_line > 0 && !ft_strchr(stash, '\n'))
+	{
+		read_line = read(fd, buffer, BUFFER_SIZE);
+		if (read_line < 0)
+			return (free(buffer), NULL);
+		else if (read_line == 0)
+			break ;
+		buffer[read_line] = '\0';
+		temp = ft_strjoin(stash, buffer);
+		free(stash);
+		stash = temp;
+	}
+	free(buffer);
+	return (stash);
 }
 
-/* Esta función construirá la línea a partir de los datos leídos. 
-Manejará la concatenación y la gestión de la memoria. Debería actualizar stash con los datos no utilizados*/
-
-char	*ft_set_line(char *line)
+static char	*ft_set_line(char *stash)
 {
+	char	*line;
+	char	*new_line;
 
+	new_line = ft_strchr(stash, '\n');
+	if (new_line != NULL)
+	.......
 }
 
-/* función principal que será llamada para obtener la siguiente línea del archivo. 
-Se encargará de coordinar el flujo del programa, gestionar el stash y llamar a las otras funciones según sea necesario.*/
-
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-	static char	*stash; // para almacenar los datos leídos, los que no fueron procesados en la primera llamada
-	char		*line; // para almacenar la línea que se devolverá
+	static char	*stash;
+	char		*line;
 
 	if (fd < 0 || BUFFER_SIZE < 0)
 		return (NULL);
-	line = ft_read_line(fd, *stash); // Lee una línea del descriptor de archivo y utiliza el contenido almacenado en stash. El resultado se almacena en el puntero line
-	if (!line)
-		return (NULL); // Si devuelve NULL indica que hubo un error al leer la línea o llegó al final del archivo
-	stash = ft_set_line(line); // Toma la línea leída y actualiza stash con ella. stash podría estar almacenando el contenido restante que no se ha procesado todavía o acumulando más datos para las siguientes lecturas.
+	stash = ft_read_line(fd, stash);
 	if (!stash)
 		return (NULL);
-	return (line); // Si todo ha ido bien, devuelve la línea leída
-}
-
-
-
-/* 
-	line = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	line = ft_set_line(stash);
 	if (!line)
 		return (NULL);
-		
-*/
+	free(stash); // ????
+	return (line);
+}
