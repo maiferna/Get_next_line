69af00f55a19ef9c664758f954706251b0305537
maiferna <maiderfernandez57@gmail.com>
2024-10-22 15:02:02 +0200

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -16,9 +16,16 @@
 # include <unistd.h>
 # include <fcntl.h>
 # include <stdlib.h>
+# include <stdio.h>
 
 # ifndef BUFFER_SIZE
-# define BUFFER_SIZE 32
+#  define BUFFER_SIZE 32
 # endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s1);
 
 #endif
