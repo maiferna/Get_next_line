f6e30175da9ae2a01813b4fe6f0ee1b6610a5967
maiferna <maiderfernandez57@gmail.com>
2024-10-24 11:00:58 +0200

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -27,5 +27,6 @@ size_t	ft_strlen(const char *str);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s1);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
